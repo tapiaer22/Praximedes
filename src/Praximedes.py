467b9af6f8b0f64e7cd6ca6c485848d4b65b20ee
tapiaer22 <tapiaer.22@gmail.com>
2024-09-09 22:14:52 -0400

    Created src

--- src/Praximedes.py
+++ src/Praximedes.py
@@ -1,7 +1,7 @@
 #For async functionalities
 import asyncio, os
 #Classes for Praximedes tools
-from Spotify_Controller import Spotify_Controller
+from src.Spotify_Controller import Spotify_Controller
 import spotipy
 from LED_Source import LED_Source
 import speech_recognition as sr, pyttsx3

497a2733da6268730f9dd014dd5b248069f82e82
tapiaer22 <tapiaer.22@gmail.com>
2024-09-09 22:09:04 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+#For modules
+import os
+import sys
+#For async functionalities
+import asyncio
+from PyQt5.QtWidgets import QApplication
+import qasync
+#Classes for Praximedes tools
+from Praximedes import Praximedes
+#For Logging
+import traceback, logging
+#For ML
+import re
+#For saved data, settings, and other functionalities
+import json, random

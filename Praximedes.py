0a9dd0a19168f996bff2ea913e995eccbff78aa7
tapiaer22 <tapiaer.22@gmail.com>
2024-09-09 22:08:03 -0400

    Create Praximedes.py

---
+++ Praximedes.py
@@ -0,0 +1,11 @@
+#For async functionalities
+import asyncio, os
+#Classes for Praximedes tools
+from Spotify_Controller import Spotify_Controller
+import spotipy
+from LED_Source import LED_Source
+import speech_recognition as sr, pyttsx3
+#For Logging
+import logging
+#For saved data, settings, and other functionalities
+import json

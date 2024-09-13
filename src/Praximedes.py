6c97aa40835f89bfc3ac34a9133c5ec4d37366d7
tapiaer22 <tapiaer.22@gmail.com>
2024-09-13 01:19:50 -0400

    Update Praximedes.py

--- src/Praximedes.py
+++ src/Praximedes.py
@@ -1,7 +1,8 @@
 #For async functionalities
 import asyncio, os
+import random
 #Classes for Praximedes tools
-from src.Spotify_Controller import Spotify_Controller
+from Spotify_Controller import Spotify_Controller
 import spotipy
 from LED_Source import LED_Source
 import speech_recognition as sr, pyttsx3
@@ -9,3 +10,16 @@ import speech_recognition as sr, pyttsx3
 import logging
 #For saved data, settings, and other functionalities
 import json
+
+#Praximedes voice assistant class
+class Praximedes:
+    def __init__():
+        pass
+
+    #Say a message
+    def speak(self, message=""):
+        pass
+    
+    #Message after succesfully executing a command
+    def confirm_command_message(command):
+        pass
